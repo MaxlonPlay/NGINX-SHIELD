26bb38a51770869e2fc0c5724527cc42054e591f
MaxlonPlay <maxlonplay779@gmail.com>
2026-02-09 10:26:14 +0100

    Enable allowedHosts in Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: true,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
